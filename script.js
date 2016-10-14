f36aaa6c91390a8e2c79ef4043986e9768682c60
katya20 <kpanchenko21@gmail.com>
2016-10-14 09:40:29 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var hello = ["Hello there!", "Nice to meet you!", "Hi", "I love you <3", "I am awesome!", "Click me!"]
 
 $(".bubble").hide();
-console.log(hello[Math.floor(Math.random() * 5)])
 
 $("#pusheen").click(doSomething)
 
 function doSomething() {
   console.log("clicked");
   $(".bubble").show();
+  $(".bubble").append(hello[Math.floor(Math.random() * 5)])
 }
 
